322912ce8163827f4f623a8a1868582366a60e81
Lunarmagpie <bambolambo0@gmail.com>
2023-02-13 16:41:29 -0500

    update help message

--- bot/plugins/util.py
+++ bot/plugins/util.py
@@ -13,7 +13,8 @@ HELP_MESSAGE = (
     f"Hi! My name is {config.NAME}, and my job is to run code."
     "\nYou can run the code in a message with a code block code by using the"
     "`Run Code` message command. Alternatively you can prefix your message with"
-    " `./run`."
+    " `./run`. Asembaly can be inspected with the `Assembly` command or the"
+    " `./asm` message prefix."
 )
 
 
