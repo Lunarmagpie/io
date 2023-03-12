b1009c7489471d000c149a8d76e28877d4a281ff
Lunarmagpie <bambolambo0@gmail.com>
2023-03-12 03:43:00 -0400

    fix delete button

--- bot/buttons/delete.py
+++ bot/buttons/delete.py
@@ -7,6 +7,10 @@ __all__: list[str] = ["delete_button"]
 @flare.button(style=hikari.ButtonStyle.DANGER, emoji="🗑️")
 async def delete_button(ctx: flare.MessageContext, author: hikari.Snowflake) -> None:
     if ctx.author.id != author:
-        await ctx.respond("Only the person that used the command can delete me.")
+        await ctx.respond(
+            "Only the person that used the command can delete me.",
+            flags=hikari.MessageFlag.EPHEMERAL,
+        )
+        return
 
     await ctx.message.delete()
